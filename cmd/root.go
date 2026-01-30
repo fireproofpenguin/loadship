d65718a765004b3d2bbf02ff6f968acfac74e54a
Sam <46286293+fireproofpenguin@users.noreply.github.com>
2026-01-30 13:36:35 +0000

    Added CLI start

---
+++ cmd/root.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "loadship",
+	Short: "Loadship is a performance and load test runner for docker-based http services",
+	Long: `Loadship is a CLI library to perform performance and load testing for docker-based http services.
+
+Loadship runs multiple http clients in parallel containers to generate load against a target service and monitors resource usage on the target service's host.
+
+loadship run http://localhost:8080`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.loadship.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
